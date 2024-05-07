89feab0de77400858ed9b70f78a3f2283142eafc
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2024-05-07 14:25:09 +0200

    add subgraph deploy:prepare-manifest script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 mkdir /app/out
 
 # deploy voucher
+echo "deploying iexec-voucher-contracts"
 cd /app/iexec-voucher-contracts
 git log -1 > /app/out/iexec-voucher-contracts.git-log
 npx hardhat run deploy/deploy.ts --network bellecour-fork
@@ -10,15 +11,15 @@ cp /app/iexec-voucher-contracts/VoucherHub.address /app/out/
 cp /app/iexec-voucher-contracts/VoucherHub.block /app/out/
 
 # upgrade poco
+echo "deploying PoCo upgrade"
 cd /app/PoCo
 git log -1 > /app/out/PoCo.git-log
 npx hardhat run scripts/upgrade.ts --network bellecour-fork
 
 # deploy subgraph
-cd /app/voucher-subgraph
-cat subgraph.template.yaml \
-  | sed "s|#VOUCHER_HUB_ADDRESS#|address: \"$(cat /app/out/VoucherHub.address)\"|g" \
-  | sed "s|#VOUCHER_HUB_START_BLOCK#|startBlock: $(cat /app/out/VoucherHub.block)|g" \
-  > subgraph.yaml
+echo "deploying iexec-voucher-contracts subgraph"
+export VOUCHER_HUB_ADDRESS=$(cat /app/out/VoucherHub.address)
+export VOUCHER_HUB_START_BLOCK=$(cat /app/out/VoucherHub.block)
+npm run codegen
 npm run create
 npm run deploy
