8e923929a9bf0ec0583904be3a9900e9b12ef38a
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2024-06-25 11:38:07 +0200

    add drain test script

---
+++ test/test-scripts/helpers/drainUsersVoucher.js
@@ -0,0 +1,38 @@
+import { Contract, ZeroAddress } from "ethers";
+import {
+  VOUCHER_HUB_ADDRESS,
+  provider,
+  setBalance,
+  getVoucherManagerWallet,
+} from "./utils.js";
+import { voucherHubAbi } from "../abis/VoucherHub.js";
+
+export const drainUsersVoucher = async ({ ownerAddress, value = 1000 }) => {
+  if (!ownerAddress) {
+    throw Error("missing ownerAddress");
+  }
+
+  console.log(`Draining user ${ownerAddress} voucher`);
+
+  const voucherHubContract = new Contract(
+    VOUCHER_HUB_ADDRESS,
+    voucherHubAbi,
+    provider
+  );
+
+  const voucherAddress = await voucherHubContract.getVoucher(ownerAddress);
+
+  if (voucherAddress === ZeroAddress) {
+    throw Error(`${ownerAddress} has no voucher, cannot top-up`);
+  }
+
+  const voucherManagerWallet = await getVoucherManagerWallet();
+
+  const drainTx = await voucherHubContract
+    .connect(voucherManagerWallet)
+    .drainVoucher(voucherAddress, { gasPrice: 0 });
+  await drainTx.wait();
+  console.log(
+    `Voucher ${voucherAddress} (owned by ${ownerAddress}) drained (tx: ${drainTx.hash})`
+  );
+};
