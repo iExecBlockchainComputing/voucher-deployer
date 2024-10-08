2e811c2429ea3c1e4d1ce8612a31d4e35e40c0b4
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2024-10-08 17:29:31 +0200

    skip voucherHub deployment when VOUCHER_HUB_ADDRESS and VOUCHER_HUB_START_BLOCK are set

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,18 @@ mkdir /app/out
 
 # deploy voucher
 echo -e "\n#####################################\n# deploying iexec-voucher-contracts #\n#####################################\n"
-
-cd /app/iexec-voucher-contracts
-cat .git-log
-cp .git-log /app/out/iexec-voucher-contracts.git-log
-npx hardhat run deploy/deploy.ts --network bellecour-fork || exit 1
-
-cp /app/iexec-voucher-contracts/VoucherHub.address /app/out/
-cp /app/iexec-voucher-contracts/VoucherHub.block /app/out/
-
-cp -r /app/iexec-voucher-contracts/abis/ /app/out/
+if [ "$VOUCHER_HUB_ADDRESS" != "" ] && [ "$VOUCHER_HUB_START_BLOCK" != "" ]
+then
+    echo -e "VOUCHER_HUB_ADDRESS is set to $VOUCHER_HUB_ADDRESS\nVOUCHER_HUB_START_BLOCK is set to $VOUCHER_HUB_START_BLOCK\n  => skiping iexec-voucher-contracts deployment"
+else
+    cd /app/iexec-voucher-contracts
+    cat .git-log
+    cp .git-log /app/out/iexec-voucher-contracts.git-log
+    npx hardhat run deploy/deploy.ts --network bellecour-fork || exit 1
+    cp /app/iexec-voucher-contracts/VoucherHub.address /app/out/
+    cp /app/iexec-voucher-contracts/VoucherHub.block /app/out/
+    cp -r /app/iexec-voucher-contracts/abis/ /app/out/
+fi
 
 # upgrade poco
 echo -e "\n##########################\n# deploying PoCo upgrade #\n##########################\n"
@@ -33,8 +35,8 @@ then
     echo -e "SKIP_SUBGRAPH == true\n  => skiping subgraph deployment"
 else
     cd /app/voucher-subgraph
-    export VOUCHER_HUB_ADDRESS=$(cat /app/out/VoucherHub.address)
-    export VOUCHER_HUB_START_BLOCK=$(cat /app/out/VoucherHub.block)
+    export VOUCHER_HUB_ADDRESS=$(cat /app/out/VoucherHub.address || echo $VOUCHER_HUB_ADDRESS)
+    export VOUCHER_HUB_START_BLOCK=$(cat /app/out/VoucherHub.block || echo $VOUCHER_HUB_START_BLOCK)
     npm run codegen || exit 1
     npm run create || exit 1
     npm run deploy || exit 1
